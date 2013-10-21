15aaaa8f66ce9a18e5f8596f4a23c6f2543be421
sowmya <m.sowmya58@gmail.com>
2013-10-21 01:33:49 +0000

    Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,15 @@ class PagesController < ApplicationController
     @page = Page.new
   end
   def create
+    @page = Page.new(params[:page].permit(:name, :title))
+   if @page.save
     redirect_to pages_url
+  else
+    render 'new'
+     end
+  end
+  private
+  def page_params
+    params.require(:page).permit(:name,:title)
   end
 end
